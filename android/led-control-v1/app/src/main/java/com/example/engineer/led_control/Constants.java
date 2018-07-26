dbd248af38a724113bb0f10f1e4065e1f469738d
Mai Duong Hieu <hieu.md@workhouse.me>
2018-07-26 15:57:20 +0700

    Change endpoint

--- android/led-control-v1/app/src/main/java/com/example/engineer/led_control/Constants.java
+++ android/led-control-v1/app/src/main/java/com/example/engineer/led_control/Constants.java
@@ -1,7 +1,7 @@
 package com.example.engineer.led_control;
 
 public class Constants {
-    public static final String MQTT_BROKER_URL = "tcp://test.mosquitto.org:1883";
+    public static final String MQTT_BROKER_URL = "tcp://ec2-18-136-58-212.ap-southeast-1.compute.amazonaws.com:1883";
     public static final String PUBLISH_TOPIC = "/user001/home001/room001/led/command";
     public static final String SUBSCRIBE_TOPIC = "/user001/home001/room001/led/state";
     public static final String CLIENT_ID = "user001";
